108b1525b68d8f40a1bf52a75a9ac585bb2b8030
Thulasi Mahesh <123890253+mahesh9854@users.noreply.github.com>
2023-10-15 09:53:46 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Portfolio
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
